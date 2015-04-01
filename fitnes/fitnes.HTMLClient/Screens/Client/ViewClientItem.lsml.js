f4b7cd042207fdc2da69a889f1958e2bc3cff264
Lina92 <Kotenok_bugulma@mail.ru>
2015-04-01 20:35:00 +0300

    3

---
+++ fitnes/fitnes.HTMLClient/Screens/Client/ViewClientItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewClientItem.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.ClientItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.ClientItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
