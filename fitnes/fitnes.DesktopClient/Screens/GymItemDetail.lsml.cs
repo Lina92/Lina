97fca448e78c8c3840bef243036561640e711803
Lina92 <Kotenok_bugulma@mail.ru>
2015-04-09 13:31:59 +0300

    22

---
+++ fitnes/fitnes.DesktopClient/Screens/GymItemDetail.lsml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class GymItemDetail
+    {
+        partial void GymItem_Loaded(bool succeeded)
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.GymItem);
+        }
+
+        partial void GymItem_Changed()
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.GymItem);
+        }
+
+        partial void GymItemDetail_Saved()
+        {
+            // Добавьте сюда свой код.
+            this.SetDisplayNameFromEntity(this.GymItem);
+        }
+    }
+}
